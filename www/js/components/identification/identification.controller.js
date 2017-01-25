ec4778078603dbe0b406636b160d010f728611e2
Alejandro Gómez Morón <amoron@emergya.com>
2017-01-26 00:01:52 +0100

    Customizing the identification module

--- www/js/components/identification/identification.controller.js
+++ www/js/components/identification/identification.controller.js
@@ -11,88 +11,117 @@
     angular.module(moduleName)
       .controller('IdentificationController', IdentificationController);
     /* @ngInject */
-    function IdentificationController($state, $ionicPopup, SE_LEG_VIEWS, UtilsFactory, MainFactory) {
+    function IdentificationController($ionicPopup, $translate, $scope, $state, SE_LEG_VIEWS, UtilsFactory, MainFactory,
+      DataFactory) {
 
       var vm = this;
+
       // Public attributes
-      vm.nationaIdNumber = undefined;
-      vm.d = undefined;
-      vm.m = undefined;
-      vm.y = undefined;
-      vm.connected = true;
       var inputIDSelector = 'new-col-input';
 
       // Public methods
       vm.send = send;
-      vm.correctFormat = correctFormat;
+      vm.correctFormat = defaultCorrectFormat;
       vm.hasInternetConnection = hasInternetConnection;
+
       activate();
+      $scope.$on('$ionicView.enter', onEnter);
       function activate() {
-        /*
-         //Check connection here
-         */
-        if (navigator && navigator.connection && navigator.connection.type) {
-          if (navigator.connection.type === 'none') {
-            vm.connected = false;
+      }
+
+      /**
+       * Method to be executed once the user accesses to the scanner component.
+       */
+      function onEnter() {
+        // clearing previous data
+        DataFactory.clear(SE_LEG_VIEWS.ID);
+        if ($state.params && $state.params.data) {
+          // initialization of the parameters
+          if ($state.params.formatHandler) {
+            vm.correctFormat = $state.params.formatHandler;
           }
         }
-        if (!vm.connected) {
+
+        if (!hasInternetConnection()) {
           $ionicPopup.alert({
             title: $translate.instant('connection.title'),
             template: $translate.instant('connection.template')
           });
         }
-
       }
+
+
       /**
        * Send identification.
        */
       function send() {
-        if (vm.nationaIdNumber) {
-          //$state.go(SE_LEG_VIEWS.FINGERPRINT, {nin: vm.nationaIdNumber, qr: $state.params.data.scanner});
+        if (vm.nationaIdNumber && vm.correctFormat()) {
+          // saving the associated data
+          DataFactory.save(SE_LEG_VIEWS.ID, vm.nationaIdNumber);
           MainFactory.handleNextComponent();
         }
       }
 
-
-      function isLeapYear() {
-        if (vm.y % 4 == 0 && (vm.y % 100 != 0 || vm.y % 400 == 0))
-          return true;
-        else
-          return false;
-      }
+      /////////////////////
+      // Private methods //
+      /////////////////////
 
       /**
-       *
+       * Method that checks if it a leap year.
+       * @param year to be checked.
+       * @return {boolean} is a leap year.
        */
-      function isCorrectDate() {
-        var arrayDiasMes = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        if (vm.y <= 1600 || vm.y >= 9999)
-          return false;
-        if (vm.m <= 0 || vm.m > 12)
-          return false;
-        if (vm.d <= 0 || vm.d > 31)
-          return false;
-        if (vm.m == 2 && vm.d > 29)
-          return false;
-        if (arrayDiasMes[vm.m - 1] < vm.d)
-          return false;
-        if (vm.m == 2 && vm.d == 29 && !isLeapYear())
-          return false;
-        return true;
+      function isLeapYear(year) {
+        var isLeap = false;
+        if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+          isLeap = true;
+        }
+        return isLeap;
       }
 
       /**
-       *
+       * It checks if the given date is valid.
+       * @param year to be checked.
+       * @param month to be checked.
+       * @param day to be checked.
+       * @return true if the date is correct.
+       */
+      function isCorrectDate(year, month, day) {
+        var correct = true;
+        var daysPerMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        if (year <= 1600 || year >= 9999) {
+          correct = false;
+        } else
+        if (correct && (month <= 0 || month > 12)) {
+          correct = false;
+        }
+        if (correct && (day <= 0 || day > 31)) {
+          correct = false;
+        }
+        if (correct && (month === 2 && day > 29)) {
+          correct = false;
+        }
+        if (correct && (daysPerMonth[month - 1] < day)) {
+          correct = false;
+        }
+        if (correct && (month === 2 && day === 29 && !isLeapYear(year))) {
+          correct = false;
+        }
+        return correct;
+      }
+
+      /**
+       * It checks if the given format is valid.
+       * @param format to be checked.
+       * @return true if the format is valid.
        */
       function isValidFormat(format) {
         if (format.length === 12) {
-
-          vm.d = parseInt(format.substr(6, 2));
-          vm.m = parseInt(format.substr(4, 2));
-          vm.y = parseInt(format.substr(0, 4));
-          vm.number = format.substr(8, 4);
-          if (isCorrectDate() && UtilsFactory.isNumber(vm.number)) {
+          var day = parseInt(format.substr(6, 2));
+          var month = parseInt(format.substr(4, 2));
+          var year = parseInt(format.substr(0, 4));
+          var idNumber = format.substr(8, 4);
+          if (isCorrectDate(year, month, day) && UtilsFactory.isNumber(idNumber)) {
             return true;
           }
         }
@@ -100,16 +129,19 @@
       }
 
       /**
-       *
+       * By default format method.
+       * @return true if the format is valid.
        */
-      function correctFormat() {
-        if (vm.connected && !UtilsFactory.isEmpty(vm.nationaIdNumber)) {
+      function defaultCorrectFormat() {
+        if (hasInternetConnection() && !UtilsFactory.isEmpty(vm.nationaIdNumber)) {
           var nationaIdNumber = vm.nationaIdNumber.toString();
           if (isValidFormat(nationaIdNumber)) {
+            // needed to auto-open the keyboard
             document.getElementById(inputIDSelector).blur();
             return true;
           }
         }
+        // needed to auto-open the keyboard
         document.getElementById(inputIDSelector).focus();
         return false;
       }
