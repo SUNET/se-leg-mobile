5ec4f6ad2a6868f684ab5bbb367e27db492e3b78
Alejandro Gómez Morón <amoron@emergya.com>
2017-01-27 20:41:36 +0100

    Creating custo method with the se-leg data process until we merge with Nacho's implementation

---
+++ www/js/core/modules/sender/sender.custom.process.data.method.js
@@ -0,0 +1,21 @@
+/**
+ * SE-LEG strategy to process data.
+ *
+ */
+var processedData = '';
+
+var savedData = DataFactory.getAll();
+
+// getting the info we need
+var preProcessedData = {};
+// getting the id
+preProcessedData.identity = savedData[SE_LEG_VIEWS.ID];
+preProcessedData.qrcode = savedData[SE_LEG_VIEWS.SCANNER];
+
+// transforming into querystring
+processedData = UtilsFactory.jsonToQueryString(preProcessedData);
+
+// replacing spaces
+processedData = processedData.split(' ').join('');
+
+return processedData;
