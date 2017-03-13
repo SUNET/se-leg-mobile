27eb74da7aeef14efd937c2798651e513fa915e0
natete <natete981@gmail.com>
2017-03-13 09:21:06 +0100

    Adding about view

--- www/js/widgets/header/header.controller.js
+++ www/js/widgets/header/header.controller.js
@@ -1,18 +1,19 @@
 /**
- * Fingerprint controller
+ * Header menu controller
  * @param {type} angular
  * @author Ignacio González <igonzalez@emergya.com>
  */
 
 (function () {
-  define(['./header.module'], function (moduleName) {
+  define(['./header.module', 'text!./about.html'], function (moduleName, aboutTemplate) {
     'use strict';
     angular
       .module(moduleName)
       .controller('SeLegHeaderController', SeLegHeaderController);
 
     /* @ngInject */
-    function SeLegHeaderController(CORE_CONFIGS) {
+    function SeLegHeaderController($ionicPopover, CORE_CONFIGS) {
+      var version;
 
       var vm = this;
       vm.hasHeader = CORE_CONFIGS.HAS_HEADER;
@@ -20,6 +21,7 @@
       vm.isMenuOpened =false;
 
       vm.openOption = openOption;
+      vm.openAbout = openAbout;
 
       activate();
 
@@ -27,7 +29,10 @@
        * Needed activation functionalities.
        */
       function activate() {
-
+        cordova.getAppVersion.getVersionNumber()
+          .then(function (result) {
+            version = result;
+          });
       }
 
       function openOption(option) {
@@ -37,6 +42,18 @@
         cordova.InAppBrowser.open(option.url, '_blank', 'location=yes');
       }
 
+      function openAbout() {
+        vm.popover = $ionicPopover.fromTemplate(aboutTemplate, {
+          scope: {
+            closeAbout: function () {
+              vm.popover.remove();
+            },
+            version: version
+          }
+        });
+
+        vm.popover.show();
+      }
     }
   });
 })();
